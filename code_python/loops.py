12aa8213b74d3d89439d4933efd201f2780217d8
Dhruvi Sheth <162569215+dssheth@users.noreply.github.com>
2024-06-29 20:56:46 +0530

    Added 4 files

---
+++ code_python/loops.py
@@ -0,0 +1,17 @@
+# while loop
+ 
+# i = 1
+
+# while i <= 5:
+    # print (i)
+    # i = i + 1
+
+# while (i <= 5):
+#     print (i * "*")
+#     i = i + 1
+
+i = 5 
+while i >= 0:
+     print (i * "*")
+     i = i - 1
+    
